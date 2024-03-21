1b5be4f23f5b5bf5d0033bd250cc87e82f7b6316
Colby <colbyx20@gmail.com>
2024-03-21 17:11:40 -0400

    added .dockerignore and .gitignore files

--- main.go
+++ main.go
@@ -4,12 +4,33 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
+type Data struct {
+	id int
+}
+
 func main() {
+	DataStore := Data{
+		id: 0,
+	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello from Docker!!!")
+	})
+
+	mux.HandleFunc("GET /task/{id}", func(w http.ResponseWriter, r *http.Request) {
+		idstr := r.PathValue("id")
+		id, err := strconv.Atoi(r.PathValue("id"))
+
+		DataStore.id = id
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(w, "Hanlding task with id=%v\n", idstr)
 	})
 
 	fmt.Println("Listening....")
